917034751776e517fc57259fcf9006b0958dd678
Devin Li <51217000+butterriolu@users.noreply.github.com>
2022-08-31 18:25:38 -0400

    changes from ssh connection

--- src/App.js
+++ src/App.js
@@ -137,13 +137,6 @@ function App() {
                   minutes of stretches before going to bed, I usually have no
                   problems sleeping until morning. Just like a baby, I wake up
                   without any fatigue or stress in the morning.
-                  <br />I was told there were no issues at my last check-up. I'm
-                  trying to explain that I'm a person who wishes to live a very
-                  quiet life. I take care not to trouble myself with any
-                  enemies, like winning and losing, that would cause me to lose
-                  sleep at night. That is how I deal with society, and I know
-                  that is what brings me happiness. Although, if I were to fight
-                  I wouldn't lose to anyone.
                 </div>
               </div>
 
@@ -177,7 +170,7 @@ function App() {
               ></div>
 
               <div style={{ fontSize: "2rem" }}>
-                Dio Brando's: You're approaching me?
+                You're approaching me?
               </div>
               <div
                 style={{
